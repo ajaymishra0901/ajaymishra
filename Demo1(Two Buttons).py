d2d9328e9640a38eae1f1dea2f3d1b443acc430e
ajaymishra0901 <54370352+ajaymishra0901@users.noreply.github.com>
2020-02-06 10:21:51 +0530

    Add files via upload

---
+++ Demo1(Two Buttons).py
@@ -0,0 +1,10 @@
+from tkinter import *
+root = Tk()
+def callback():
+     print("You Clicked Left Button....i.e (One)")
+def callback2():
+     print("You Clicked Right Button....i.e (Two)")
+button1= Button(root,text="ONE",command=callback,bg="Green",fg="RED")
+button2= Button(root,text="TWO",command=callback2,bg="Red",fg="Green")
+button1.pack(side=LEFT)
+button2.pack(side=RIGHT)
